eebe3bbbaccb3e3b528563988ab4dd398b2ea4e1
Frank Maxwitat <maxwitat@gmx.de>
2023-03-01 09:57:11 +0100

    Add files via upload

---
+++ SophosRemovalDetectionScript.ps1
@@ -0,0 +1,14 @@
+﻿$SophosUninst = "C:\Program Files\Sophos\Sophos Endpoint Agent\uninstallcli.exe"
+if ((Test-Path -Path $SophosUninst) -eq $true) {
+    exit 1
+}
+else {
+    $SophosUninstx86 = "C:\Program Files (x86)\Sophos\Sophos Endpoint Agent\uninstallcli.exe"
+    if ((Test-Path -Path $SophosUninstx86) -eq $true) {
+        exit 1
+    }
+    else {
+       Write-Output 'No Sophos Endpoint found'
+       exit 0
+    }
+}
